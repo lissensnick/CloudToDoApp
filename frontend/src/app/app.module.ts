b41b1038269f2ac6f496f0156cb2bf89800a079d
lissensnick <91500297+lissensnick@users.noreply.github.com>
2022-12-30 20:34:35 +0100

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,6 +9,25 @@ import { AddTodoComponent } from './components/add-todo/add-todo.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { CarrouselComponent } from './components/carrousel/carrousel.component';
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+    applicationId: 'a5a3d80c-3cc5-439b-babb-52cd99e24ae5',
+    clientToken: 'pubf9435a04edbbc57d390c0a1fe8bc1957',
+    site: 'datadoghq.com',
+    service:'pecloud',
+    
+    // Specify a version number to identify the deployed version of your application in Datadog 
+    // version: '1.0.0',
+    sampleRate: 100,
+    sessionReplaySampleRate: 20,
+    trackInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel:'mask-user-input'
+});
+    
+datadogRum.startSessionReplayRecording();
 
 @NgModule({
   declarations: [
